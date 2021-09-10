fd1f1c0843fff11f85729f810d868dc690dabf23
Andy Wyatt <andy.wyatt@europe.svpworldwide.com>
2021-09-10 16:48:25 +0100

    Add Xamarin test project.

---
+++ bson-exports-in-xamarin/bson-c-xamarin/bson-c-xamarin/BsonExports.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace bson_c_xamarin
+{
+  /*
+  extern "C" BsonResult BsonExport(char* ptr, uint32_t size);
+  extern "C" void BsonResultFree(char* ptr);
+  */
+  internal static class BsonExports
+  {
+// #if Android
+#if ! IOS
+    const string DllName = "libbson_exports.so";
+#else
+    const string DllName = "__Internal";
+#endif
+
+    public struct BsonResult
+    {
+      public IntPtr ptr;
+      public UInt32 len;
+    }
+
+    [DllImport(DllName, EntryPoint = "BsonExport")]
+    internal static extern BsonResult BsonExport(IntPtr ptr, UInt32 size);
+
+    [DllImport(DllName, EntryPoint = "BsonResultFree")]
+    internal static extern void BsonResultFree(IntPtr ptr);
+  }
+}
---
+++ bson-exports-in-xamarin/bson-c-xamarin/bson-c-xamarin/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+#if Android
+using bson_c_xamarin.Droid;
+#endif
+
+namespace bson_c_xamarin
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+      var result = BsonExports.BsonExport(IntPtr.Zero, 0);
+    }
+  }
+}
